1a9f11ac9e870fbedc2cb51ea5f82ade01d067ea
ADMIN <ADMIN@DESKTOP-S78URNJ>
2023-05-02 17:35:37 +0530

    second commit...

--- SampleGitProject/src/com/today/Demo1.java
+++ SampleGitProject/src/com/today/Demo1.java
@@ -5,9 +5,8 @@ public class Demo1 {
 	public static void main(String[] args) {
 		
 		
-		System.out.println("this is simple java....");
-		
-		System.out.println("this is second line.....");
+		System.out.println("this is first line....");
+		System.out.println("this is second line....");
 	}
 
 }
