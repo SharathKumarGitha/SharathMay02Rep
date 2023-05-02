e6f3fe72386a72863fc4486691ae71ac8bb03c08
ADMIN <ADMIN@DESKTOP-S78URNJ>
2023-05-02 17:32:27 +0530

    first commit

---
+++ SampleGitProject/src/com/today/Demo1.java
@@ -0,0 +1,13 @@
+package com.today;
+
+public class Demo1 {
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println("this is simple java....");
+		
+		System.out.println("this is second line.....");
+	}
+
+}
