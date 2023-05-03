5b208004aa1eea4b49b664cc90b18f678b6ca2e1
sharathgitha627 <sharathgitha1627@gmail.com>
2023-05-03 14:39:17 +0530

    qqqqq

--- SampleGitProject/src/com/today/Demo1.java
+++ SampleGitProject/src/com/today/Demo1.java
@@ -20,8 +20,8 @@ public class Demo1 {
 		System.out.println("this is 9th line......");
 		System.out.println("this is 10th line.....");
 		
-		System.out.println("last1");
-		System.out.println("last2");
+		System.out.println("last1111");
+		System.out.println("last2111");
 
 		
 	}
