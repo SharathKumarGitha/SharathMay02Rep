c854aaf3430171b870ad782489f76318b4d5d467
sharathgitha627 <sharathgitha1627@gmail.com>
2023-05-03 13:28:32 +0530

    1111 & 2222 added....

--- SampleGitProject/src/com/today/Demo1.java
+++ SampleGitProject/src/com/today/Demo1.java
@@ -20,8 +20,8 @@ public class Demo1 {
 		System.out.println("this is 9th line......");
 		System.out.println("this is 10th line.....");
 		
-		System.out.println("1");
-		System.out.println("2");
+		System.out.println("1111");
+		System.out.println("2222");
 		
 	}
 
