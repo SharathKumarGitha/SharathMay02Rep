92190d5c4eaa2438e6c5bff621d5f3e47df8e8c6
sharathgitha627 <sharathgitha1627@gmail.com>
2023-05-03 12:17:43 +0530

    1 & 2 added....

--- SampleGitProject/src/com/today/Demo1.java
+++ SampleGitProject/src/com/today/Demo1.java
@@ -20,8 +20,8 @@ public class Demo1 {
 		System.out.println("this is 9th line......");
 		System.out.println("this is 10th line.....");
 		
-		System.out.println("these are new line and line is 11th line....");
-		System.out.println("these are new line and line is 12th line....");
+		System.out.println("1");
+		System.out.println("2");
 		
 	}
 
